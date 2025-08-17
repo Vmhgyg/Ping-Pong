1668a2f2b277f294052877e3ff4bfd720e03caec
Vmhgyg <lauyiichern06@gmail.com>
2025-08-17 15:28:35 +0800

    add license, update readme, create pingpong.py

---
+++ ping_pong.py
@@ -0,0 +1,31 @@
+from pygame import *
+
+
+#Window and initiation
+
+length = 700
+width = 500
+
+
+Window = display.set_mode((length,width))
+display.set_caption("Pong")
+
+clock = time.Clock()
+
+
+FPS = 60
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
